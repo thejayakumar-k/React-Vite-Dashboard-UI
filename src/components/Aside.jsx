ef024fc150df639173a2835cdab47d046c067caf
JAYAKUMAR.K <kjaya3331@gmail.com>
2026-04-28 14:49:22 +0530

    Update Aside.jsx

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -21,15 +21,18 @@ const links = [
 
 const Aside = () => {
   return (
-    <aside className=' p -6 bg-gray-200 rounded shadow-md p-4'>
-        <h2 className='text-xl font-bold'>📌 Related Links</h2>
-        <ul className='space-y-2 mt-2'>
+    <aside className='p-4 md:p-6 bg-gray-200 rounded shadow-md w-full md:w-1/3'>
+        <h2 className='text-lg md:text-xl font-bold'>📌 Related Links</h2>
+        <ul className='space-y-2 mt-2 text-sm md:text-base'>
             {
                 links.map((item, index) => (
-                    <li><a className='flex items-center text-blue-500 gap-1 hover:underline' href={item.href}>{item.icon} {item.name}</a></li>
+                    <li key={index}>
+                        <a className='flex items-center text-blue-500 gap-1 hover:underline' href={item.href}>
+                            {item.icon} {item.name}
+                        </a>
+                    </li>
                 ))
             }
-            
         </ul>
     </aside>
   )
