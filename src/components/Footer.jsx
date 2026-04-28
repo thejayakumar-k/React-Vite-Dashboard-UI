43a2fe6e7c02148b9927b19791db9f82339b1b48
JAYAKUMAR.K <kjaya3331@gmail.com>
2026-04-28 14:49:51 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-gray-900 text-white text-center py-6'>
-        <p>🌎&copy; 2026 MyWebsite. All Rights Reserved.</p>
-        <div className='flex justify-center space-x-2 mt-4'>
+    <footer className='bg-gray-900 text-white text-center py-6 px-4'>
+        <p className='text-sm md:text-base'>🌎&copy; 2026 MyWebsite. All Rights Reserved.</p>
+        <div className='flex flex-col md:flex-row justify-center items-center gap-2 md:space-x-4 mt-4 text-sm md:text-base'>
             <a href="#" className='hover:text-blue-600'>ⓕ Facebook</a>
             <a href="#" className='hover:text-blue-600'>🐦 Twitters</a>
             <a href="#" className='hover:text-blue-600'>📷 Instagram</a>
